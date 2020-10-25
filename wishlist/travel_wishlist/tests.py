2cdebb64ffa3e2c95d25f49a4da3ebc58c2cab1f
KJTempas <fw6927mm@go.minneapolis.edu>
2020-10-25 09:43:28 -0500

    added comments

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,20 +1,24 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.test import TestCase #extension of TestCase 
+from django.urls import reverse #to look up urls
 
 from .models import Place
-# Create your tests here.
+
 
 class TestHomePageIsEmptyList(TestCase):
 
     def test_load_home_page_shows_empty_list(self):
+        #look up url by name = place list in urls.py
         response = self.client.get(reverse('place_list'))
+        #this is an alt way to do the above line, taking two lines
+        #home_page_url = reverse('place-list') looks up url by name=place-list
+        #response = self.client.get(home_page_url])
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
         self.assertFalse(response.context['places']) 
         self.assertContains(response, 'You have no places in your wishlist')
 
 
 class TestWishList(TestCase):
-    #data will load from test_places.json fixture
+    #sample data will load from test_places.json fixture
     fixtures =['test_places']
 
     def test_view_wishlist_contains_not_visited_places(self):
@@ -54,18 +58,18 @@ class TestVisitedList(TestCase):
 class TestAddNewPlace(TestCase):
 
     def test_add_new_unvisited_place_to_wish_list(self):
-
+        #longer version
+        #add_place_url = reverse('place_list') #look up url by name
+        #new_place_data = {dictionary of data - 'name': 'Tokyo', 'visited': False}
+       #follow=true means redirect if another request follows this request
         response = self.client.post(reverse('place_list'), { 'name': 'Tokyo', 'visited': False }, follow=True)
-
         #check that correct template was used
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
-
         #what data was used to populate the template?
         response_places = response.context['places']
         #should be 1 item
         self.assertEqual(len(response_places), 1)
         tokyo_response = response_places[0]
-
         #expect this data to be in the database. Use get() to get data with 
         #the values expected.  Will throw an exception if no data, or more than
         #one row, matches.  Remember throwing an exception will cause this test to fail
@@ -77,24 +81,20 @@ class TestAddNewPlace(TestCase):
 
 class TestVisitPlace(TestCase):
 
-    fixtures = ['test_places']
-
+    fixtures = ['test_places'] #load data from fixtures file
     def test_visit_place(self):
-
         #visit place pk=2, New York (change NewYork to visited)
         response = self.client.post(reverse('place_was_visited', args=(2,) ), follow=True)
         #check that correct template was used
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
-
         #no New York in the response - so NY is not on the wishlist template anymore
         self.assertNotContains(response, "New York")
-
         #is New York visited?
         new_york = Place.objects.get(pk=2)
         self.assertTrue(new_york.visited)
 
 
-    def test_visit_no_existent_place(self):
+    def test_visit_non_existent_place(self):
 
         #visit place w pk =200 does not exist
         response = self.client.post(reverse('place_was_visited', args=(200,) ), follow=True)
